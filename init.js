a3329457dae23e149984d8c1fdd7199085d7985b
Nikhil <chauhannikhil20@gmail.com>
2021-05-03 01:24:48 +0530

    added logo

--- init.js
+++ init.js
@@ -1,25 +1,24 @@
 let topRow = document.querySelector(".top-row");
-        let str = "";
-        for (let i = 0; i < 26; i++) {
-            str += `<div class='col'>${String.fromCharCode(65 + i)}</div>`;
-        }
-        topRow.innerHTML = str;
-        let leftCol = document.querySelector(".left-col");
-        str = ""
-        for (let i = 0; i < 100; i++) {
-            str += `<div class='left-col_box'>${i + 1}</div>`
-        }
-        leftCol.innerHTML = str;
+let str = "";
+for (let i = 0; i < 26; i++) {
+    str += `<div class='col'>${String.fromCharCode(65 + i)}</div>`;
+}
+topRow.innerHTML = str;
+let leftCol = document.querySelector(".left-col");
+str = ""
+for (let i = 0; i < 100; i++) {
+    str += `<div class='left-col_box'>${i + 1}</div>`
+}
+leftCol.innerHTML = str;
 
-        let grid = document.querySelector(".grid");
-        str = "";
-        for (let i = 0; i < 100; i++) {
-            str += `<div class="row">`
-            for (let j = 0; j < 26; j++) {
-                str += `<div class='col' contenteditable="true" rid=${i} cid=${j}></div>`
-            }
-            str += "</div>";
-            //${String.fromCharCode(65 + j)}${i + 1}
-        }
-        grid.innerHTML = str;
-        
+let grid = document.querySelector(".grid");
+str = "";
+for (let i = 0; i < 100; i++) {
+    str += `<div class="row">`
+    for (let j = 0; j < 26; j++) {
+        str += `<div class='col' contenteditable="true" rid=${i} cid=${j}></div>`
+    }
+    str += "</div>";
+    //${String.fromCharCode(65 + j)}${i + 1}
+}
+grid.innerHTML = str;
